62124a5bc247663b1caf152ec4ea7f499d378794
laylabtrice <layla.beatrice@alunoescolassociais.org.br>
2023-07-25 12:07:09 -0300

    atualização

--- login.js
+++ login.js
@@ -15,6 +15,25 @@ function salvarUser(){
     let nomeUser = document.getElementById("nomeUser").value;
     if(nomeUser){
         dadosLista.push(nomeUser);
-        console.log(dadosLista);
+        criaLista();
+        document.getElementById("nomeUser").value = '';
     }
+}
+
+function criaLista(){
+    let tabela = document.getElementById("tabela").innerHTML = " <tr><th>Nome Usuário</th><th>Ações</th></tr>";
+
+for(let i=0; i <= (dadosLista.length-1); i++){
+    tabela += "<tr><td>" + dadosLista[i] + "</td><td><button class=' btn btn-success' onclick='editar(this.parentNode.parentNode.rowIndex)'>Editar</button><button class='btn btn-danger' onclick='excluir(this.parentNode.parentNode)'>Excluir</button></td></tr>";
+        document.getElementById('tabela').innerHTML = tabela;
+    }
+}
+function editar(i){
+    document.getElementById("nomeUser").value = dadosLista[(i - 1)];
+    dadosLista.splice(dadosLista[(i-1), 1]);
+}
+
+function excluir(i){
+    dadosLista.splice((i-1), 1);
+    document.getElementById("tabela").deleteRow(i);
 }
